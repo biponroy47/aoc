ae80c57f2df410ab63544e43b4bfa7f9826758ee
biponroy47 <biponroy47@yahoo.com>
2022-12-04 23:57:14 -0500

    day 5 initial commit

---
+++ day5/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("input2.txt");
+		Scanner reader = new Scanner(input);
+		
+	}
+	
+}
