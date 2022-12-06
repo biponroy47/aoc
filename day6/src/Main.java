c1cc337d4e4ddce149772fb865869b7ab4f9d8a2
biponroy47 <biponroy47@yahoo.com>
2022-12-06 00:40:21 -0500

    day 6 solution

--- day6/src/Main.java
+++ day6/src/Main.java
@@ -8,10 +8,26 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		File input = new File("input2.txt");
 		Scanner reader = new Scanner(input);
+		String[] chars = reader.nextLine().split("");
 
-		while (reader.hasNextLine()) {
-			String[] inputs = reader.nextLine().split(" ");
-
+		search: for (int i = 0; i < chars.length; i++) {
+			ArrayList<String> list = new ArrayList<String>();
+			for(int j = i; j < (i + 14); j++) {
+				if (!list.contains(chars[j])) list.add(chars[j]);
+			}
+			if (list.size() == 14) {
+				System.out.println(i + 14);
+				break search;
+			}
+//			part 1
+//			if (!list.contains(chars[i])) list.add(chars[i]);
+//			if (!list.contains(chars[i + 1])) list.add(chars[i + 1]);
+//			if (!list.contains(chars[i + 2])) list.add(chars[i + 2]);
+//			if (!list.contains(chars[i + 3])) list.add(chars[i + 3]);
+//			if (list.size() == 4) {
+//				System.out.println(i + 4);
+//				break search;
+//			}
 		}
 	}
 }
