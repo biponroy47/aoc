92e1ccbb3e1a07b64c0dcea2c124015c16793f33
biponroy47 <biponroy47@yahoo.com>
2022-12-06 01:35:11 -0500

    day 3, using hashmap instead:

--- day6/src/Main.java
+++ day6/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -9,25 +10,14 @@ public class Main {
 		File input = new File("input2.txt");
 		Scanner reader = new Scanner(input);
 		String[] chars = reader.nextLine().split("");
-
+		
 		search: for (int i = 0; i < chars.length; i++) {
-			ArrayList<String> list = new ArrayList<String>();
-			for(int j = i; j < (i + 14); j++) {
-				if (!list.contains(chars[j])) list.add(chars[j]);
-			}
+			HashSet<String> list = new HashSet<String>();
+			for (int j = i; j < (i + 14); j++) list.add(chars[j]);
 			if (list.size() == 14) {
 				System.out.println(i + 14);
 				break search;
 			}
-//			part 1
-//			if (!list.contains(chars[i])) list.add(chars[i]);
-//			if (!list.contains(chars[i + 1])) list.add(chars[i + 1]);
-//			if (!list.contains(chars[i + 2])) list.add(chars[i + 2]);
-//			if (!list.contains(chars[i + 3])) list.add(chars[i + 3]);
-//			if (list.size() == 4) {
-//				System.out.println(i + 4);
-//				break search;
-//			}
 		}
 	}
 }
