7fbaaf145dbb1938155f56f6756abcd6563a46c1
biponroy47 <biponroy47@yahoo.com>
2022-12-06 23:58:24 -0500

    day 7 initial commit

---
+++ day7/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("input2.txt");
+		Scanner reader = new Scanner(input);
+		
+		
+	}
+}
