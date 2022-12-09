c94eb94afa861de635d1c1ada382031772800dce
biponroy47 <biponroy47@yahoo.com>
2022-12-09 00:39:54 -0500

    day 8 initial commit

---
+++ day8/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File input = new File("input.txt");
+		Scanner reader = new Scanner(input);
+
+	}
+
+}
